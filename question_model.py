d762988d2f8f6828dd1a13706cd3122ee49a11c7
rizwanlodhi <rizwan.lodhi@gmail.com>
2026-01-11 18:01:02 -0600

    Fully functional app.

--- question_model.py
+++ question_model.py
@@ -1,6 +1,5 @@
-# This is a class
 class Question:
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
 
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
